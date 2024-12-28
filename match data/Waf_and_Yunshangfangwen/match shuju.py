fef2a4616e7548ea9c99742f69e68e2564c74b9a
Haojie <1210565885@qq.com>
2024-12-28 11:45:40 +0800

    修改WAF与云上访问日志匹配方式

--- match data/Waf_and_Yunshangfangwen/match shuju.py
+++ match data/Waf_and_Yunshangfangwen/match shuju.py
@@ -1,15 +1,12 @@
 import json
 
-
 def load_json(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         return [json.loads(line) for line in f.readlines()]
 
-
 def save_json(data, file_path):
     with open(file_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-
 
 def match_json_data(file1_data, file2_data, field1, field2):
     matched_data = []
@@ -24,11 +21,13 @@ def match_json_data(file1_data, file2_data, field1, field2):
     for item1 in file1_data:
         key = (item1[field1], item1[field2])
         if key in file2_dict:
+            result = {"WAF日志":item1,"云上访问日志":[]}
             for matched_item in file2_dict[key]:
-                matched_data.append({"WAF日志": item1, "云上访问日志": matched_item})
+                result["云上访问日志"].append(matched_item)
+
+            matched_data.append(result)
 
     return matched_data
-
 
 file1_data = load_json('C:/Users/赵豪/Desktop/工作/数据中心/可研所需资料/外网相关文件/外网云上WAF日志/2024-12-12-16-49-53_RequestLogDetail.json')
 file2_data = load_json('C:/Users/赵豪/Desktop/工作/数据中心/可研所需资料/外网相关文件/外网云上访问日志-12.09.7.00-8.30.59/2024-12-12-14-51-17_FullAccessLogDetail.json')
